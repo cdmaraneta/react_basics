5890b57c18d974b5fa518a4e7d4e5d986b70a58e
Nero <65642721+cdmaraneta@users.noreply.github.com>
2020-12-17 22:04:22 +0800

    React Aux/ Fragment
    React Aux/ Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxillary';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
+      <Aux>
+        {/* <div className={classes.Person}> */}
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
@@ -15,7 +17,8 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+        {/* </div> */}
+      </Aux>
     );
   }
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
@@ -79,7 +80,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => this.setState({ showCockpit: false })}>
           Remove Cockpit
         </button>
@@ -92,7 +93,7 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
